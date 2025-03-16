60bb3ae6fa3aef9381fbeb391d3d85ca7174d57d
KhinKhine <125184607+KhinKhine@users.noreply.github.com>
2025-03-16 14:07:02 +0900

    lp作成

---
+++ assets/js/about.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.js";/* empty css        *//* empty css    */import"./common.js";function o(){console.log("📃 ABOUT PAGE LOADED")}document.addEventListener("DOMContentLoaded",o);
